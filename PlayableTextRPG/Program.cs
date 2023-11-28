2c6333657230ec52a6d119ac63a9a207b13ee782
Ryan LeBlanc <ryanleb2003@gmail.com>
2023-11-28 18:48:52 -0400

    Some variables added

--- PlayableTextRPG/Program.cs
+++ PlayableTextRPG/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,34 @@ namespace PlayableTextRPG
 {
     internal class Program
     {
+        // player 
+        
+        private int maxplayerHealth;
+        private int playerHealth;
+
+        
+        
+        
+        // map
+        static string path;
+        static string RPGMap = @"RPGMap.txt";
+        static string[] floor;
+        static char[,] layout;
+        static int mapX;
+        static int mapY;
+        static int maxPlayerX;
+        static int maxPlayerY;
+
+        
+        
+
+
         static void Main(string[] args)
         {
+
+
         }
-    }
-}
+
+        
+        
+ 
